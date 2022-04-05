0339c5d184f4645c8f0967b5c33636d3b0e68cc0
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-04-05 17:55:30 +0200

    20220405

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
-const express = require("express")  //ezek a csomagok amike kelleni fognak
+const express = require("express");  //ezek a csomagok amike kelleni fognak
+const req = require("express/lib/request");
 const fs = require("fs")
 const path = require("path")
 
 const app = express(); //változóba mentjünk az express függvényt
 const port = 9000
 
+app.use(express.json()) //EZ mindig kell a post metódushoz!!!!!!!!!!!
+
+const fFolder = `${__dirname}/../frontend`
+app.use('/public', express.static(`${fFolder}/public`));
+const userFile = path.join(`${__dirname}/../frontend/users.json`)
+
 app.get("/", (request, response, next) => {  // "http://127.0.0.1:9000/" oldalon a functionban rakott object lesz benne 
     response.sendFile(path.join(`${__dirname}/../frontend/index.html`)); //az index elérési útvonalát itt adjuk meg
+})
+
+app.get("/admin/order-view", (request, response, next) => { 
+    response.sendFile(path.join(`${__dirname}/../frontend/index.html`)); 
 })
 
 app.get("/kismacska", (request, response, next) => {  // "http://127.0.0.1:9000/kismacska" oldalon a functionban rakott object lesz benne 
@@ -31,7 +42,7 @@ app.get("/api/v1/users", (request, response, next) => {
 /*     response.send(JSON.stringify(users))  */// ÍGy küldün json fájl a frontendnek, (JSON.stringfy-al alakítjuk át kompnak)
 })
 
-app.get("/api/v1/users/active", (request, response, next) => {
+/* app.get("/api/v1/users/active", (request, response, next) => {
     fs.readFile("../frontend/users.json", (error, data) => {  //fs = filesystem, elérési útvonal, kettő para = error, és data
         if(error){
             response.send("something error while read the file")
@@ -42,9 +53,9 @@ app.get("/api/v1/users/active", (request, response, next) => {
         }
 
     })  
-});
+}); */
 
-app.get("/api/v1/users/passive", (request, response, next) => { 
+/* app.get("/api/v1/users/passive", (request, response, next) => { 
     fs.readFile("../frontend/users.json", (error, data) => {  //fs = filesystem, elérési útvonal, kettő para = error, és data
         if(error){
             response.send("something error while read the file")
@@ -54,14 +65,85 @@ app.get("/api/v1/users/passive", (request, response, next) => {
             response.send(passiveUsers)
         }
     })  
-});
+}); */
 
 app.get("/something", (request, response, next) => {  
     console.log("request recived or something endpoint")  
     response.send("Thank you for your request, this is our respone for something endpoint")
 })
 
-app.use('/public', express.static(`${__dirname}/../frontend/public`));
+app.get("/api/v1/users-query", (request, response, next) => {  
+    console.dir(request.query.apiKey)
+    if(request.query.apiKey === "apple"){
+        response.sendFile(path.join(`${__dirname}/../frontend/users.json`));
+    }else{
+        response.send("Unauthurized request")
+    }
+    
+})
+
+app.get("/api/v1/users/:key", (request, response, next) => {  
+    console.dir(request.params)
+    console.log(request.params.key)
+    if(request.params.key === "active"){
+        response.send("You write apple")    
+    }else{
+        response.send("You dont write")
+    }
+    response.send("hello")
+    
+})
+
+app.get("/api/v1/users-params/:key", (request, response, next) => {  
+    console.dir(request.params)
+    console.log(request.params.key)
+
+    fs.readFile(userFile, (error, data) => { 
+        const users = JSON.parse(data) //javascript objektumra konvertálja az adatot
+
+        if(request.params.key === "active"){
+           
+            response.send(users.filter(users => users.status ==="active"))
+                
+        }else if(request.params.key === "passive"){
+
+            response.send(users.filter(users => users.status ==="passive"))
+
+        }else{
+
+            response.send("Something wrong")
+
+        }
+
+    })
+    
+})
+
+/* ---------------------------------------Post----------------------------------------(Az elején van egy app.use!!!) */
+
+app.post("/users/new", (req, res) => {  //Itt jelöljük ki a post
+    fs.readFile(`${fFolder}/users.json`, (error, data) => {  //Itt magát a változtatni kívánt mappát jelöljük ki
+        if(error){  //Ha nem sikerült, csókolom
+            console.log("error")
+            res.send("Error reading")
+        }else{
+            const users = JSON.parse(data) //Ha sikerül, akkor JSON fájlra konvertálja
+            console.log(req.body)
+
+            users.push(req.body)  //itt pusholja bele az új változóba
+                
+                fs.writeFile(`${fFolder}/users.json`, JSON.stringify(users), error => {  //Itt írjuk bele a fájlba,végleg
+                    if (error){
+                        console.log(error)
+                        res.send("Error van bástya")
+                    }
+                })
+                res.send(req.body)
+        }
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`)
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -12,14 +12,61 @@ const userComponent = ({name, surname}) => {
     `
 }
 
+function addUserComponent(){
+    return`
+        <div>
+            <input type="text" name="name" class="name" placeholder="first name">
+            <input type="text" name="surname"  class="surname" placeHolder="surname">
+            <button class="myButton">Send</button>
+        </div>
+    `
+}
+
 const loadEvent = async () => {
 
+    if(window.location.pathname==="/admin/order-view"){
+        console.log("We are in the admin site")
+    }else{
+        console.log("We are in the shopping site")
+    }
     const result = await pharseJSON("/api/v1/users")
     const rootElement=document.getElementById("root")
     rootElement.insertAdjacentHTML(
         "beforeend", 
         result.map(users => userComponent(users)).join("")
     )
+    rootElement.insertAdjacentHTML("afterend", addUserComponent())
+
+   /*  ----------------------------post--------------------------------- */
+
+    const button = document.querySelector(".myButton")  //Itt mentjük el a gombot, és az inputokat változókba
+    const name = document.querySelector(".name")
+    const surname = document.querySelector(".surname")
+
+    button.addEventListener('click', e => {  //gombra rakunk egy click eseményt
+        const userData = {
+            name : name.value,  //Itt adjuk meg a click eseményhez az inputokat.
+            surname: surname.value
+        };
+
+    
+
+        fetch("/users/new", {
+            method: "post",
+            headers: {
+                "Content-type" :"application/json"
+            },
+            body: JSON.stringify(userData)  
+        })
+
+        .then(async data => {
+            const user = await data.json();  //Itt adunk új felhasználókat
+
+            rootElement.insertAdjacentHTML("beforend", userComponent(user))
+
+        })
+
+    })
 
 }
 window.addEventListener("load", loadEvent)
